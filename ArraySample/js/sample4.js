ef4ab3cdb6ad2bcf6ed3cb9d84e173a465cb199b
LeoAndo <leo.777.info@gmail.com>
2021-08-28 18:22:01 +0900

    配列のpop/pushサンプル

---
+++ ArraySample/js/sample4.js
@@ -0,0 +1,14 @@
+// shift/unshift sample
+
+// unshift
+const ary1 = ['a', 'b', 'c'];
+ary1.unshift('d');
+document.write(ary1 +'<br>');
+
+// shift
+document.write(ary1.shift() + '<br>')
+document.write(ary1.shift() + '<br>')
+
+// unshift
+ary1.unshift('e');
+document.write(ary1 + '<br>')
