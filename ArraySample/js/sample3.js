cfd87de277e4b9ca5d68b012998489463bb98796
LeoAndo <leo.777.info@gmail.com>
2021-08-28 18:19:47 +0900

    配列のpop/pushサンプル

---
+++ ArraySample/js/sample3.js
@@ -0,0 +1,14 @@
+// pop / push sample
+
+// push
+const ary1 = ['a', 'b', 'c'];
+ary1.push('d');
+document.write(ary1 +'<br>');
+
+// pop
+document.write(ary1.pop() + '<br>')
+document.write(ary1.pop() + '<br>')
+
+// push
+ary1.push('e');
+document.write(ary1 + '<br>')
